34bc348186df2c6bd6074bc2f23c3631fb405f2b
Natnael Bekele <natnaelbekele142@gmail.com>
2023-12-27 13:49:39 +0300

    dag updated

--- dags/traffic_data_dag.py
+++ dags/traffic_data_dag.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.operators.bash_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 # from airflow.operators.docker_operator import DockerOperator
 from airflow.utils.dates import days_ago
@@ -34,82 +35,38 @@ run_dbt_model = BashOperator(
     dag=dag,
 )
 
-# Set the task dependencies, if any
-# Example: run_dbt_model.set_upstream(other_task)
 
-if __name__ == "__main__":
-    dag.cli()
+dag = DAG(
+    dag_id='traffic_data_dag',
+    description='DAG for processing traffic data with dbt',
+    default_args=default_args,
+    schedule_interval='@daily',
+)
 
+task_load_data = PythonOperator(
+    task_id='load_data',
+    python_callable=load_csv_to_database,
+    dag=dag,
+)
 
+task_insert_data = BashOperator(
+    task_id='insert_data',
+    bash_command='dbt run --models=insert_data',
+    dag=dag,
+)
 
-# from datetime import timedelta, datetime
-# from airflow import DAG
-# from airflow.operators.python_operator import PythonOperator
-# from airflow.operators.bash_operator import BashOperator
-# import pandas as pd
-# import os
-# from sqlalchemy import create_engine
+task_average_distance = BashOperator(
+    task_id='average_distance',
+    bash_command='dbt run --models=average_distance',
+    dag=dag,
+)
 
-# # Set the database credentials
-# username = 'postgres'
-# password = 'nati@postgres'
-# database = 'TrafficData'
-# schema = 'Public'
+task_average_speed = BashOperator(
+    task_id='average_speed',
+    bash_command='dbt run --models=average_speed',
+    dag=dag,
+)
 
-
-# csv_file_path = os.path.abspath('../data/expanded_data.csv')
-
-# # Create the SQLAlchemy engine
-# engine = create_engine(f'postgresql://{username}:{password}@localhost/{database}')
-
-# def load_csv_to_database():
-#     # Read the CSV file into a pandas DataFrame
-#     df = pd.read_csv(csv_file_path)
-
-#     # Write the DataFrame to the database table
-#     df.to_sql(name='traffic_data', con=engine, schema=schema, if_exists='replace', index=False)
-
-#     # Close the database connection
-#     engine.dispose()
-
-# default_args = {
-#     'owner': 'nathybkl',
-#     'start_date': datetime(2023, 12, 20),
-#     'retries': 5,
-#     'retry_delay': timedelta(minutes=2),
-# }
-
-# dag = DAG(
-#     dag_id='traffic_data_dag',
-#     description='DAG for processing traffic data with dbt',
-#     default_args=default_args,
-#     schedule_interval='@daily',
-# )
-
-# task_load_data = PythonOperator(
-#     task_id='load_data',
-#     python_callable=load_csv_to_database,
-#     dag=dag,
-# )
-
-# task_insert_data = BashOperator(
-#     task_id='insert_data',
-#     bash_command='dbt run --models=insert_data',
-#     dag=dag,
-# )
-
-# task_average_distance = BashOperator(
-#     task_id='average_distance',
-#     bash_command='dbt run --models=average_distance',
-#     dag=dag,
-# )
-
-# task_average_speed = BashOperator(
-#     task_id='average_speed',
-#     bash_command='dbt run --models=average_speed',
-#     dag=dag,
-# )
-
-# task_load_data >> task_insert_data
-# task_insert_data >> task_average_distance
-# task_insert_data >> task_average_speed
+task_load_data >> task_insert_data
+task_insert_data >> task_average_distance
+task_insert_data >> task_average_speed
